de021b23b85f43ee229cb37849d594c00a1d07df
Melissa Giaime <melissa.giaime@gmail.com>
2015-10-28 23:05:17 -0700

    js file in working order

--- game.js
+++ game.js
@@ -1,10 +1,14 @@
 //undeclared variables because the values will not be determined until the user inputs their answers
+
 var answer1, answer2, answer3, answer4;
 
 //starting out with zero questions correct because the values are not yet determined
+
 var total = 0;
 
-//question1, question2, question3, question 5 are all y/n -- make sure all potential correct answers are accepted using ||
+//question1, question2, question3, question 5 are all y/n --
+//make sure all potential correct answers are accepted using ||
+
 var question1 = prompt('Have I ever broken a bone?').toLowerCase();
   if (question1 === 'yes' || question1 === 'y') {
     answer1 = 'correctly. I have broken one bone.';
@@ -14,6 +18,9 @@ var question1 = prompt('Have I ever broken a bone?').toLowerCase();
     answer1 = 'wrong. I broke my coccyx jumping off a 30ft cliff into Whistle Lake in Anacortes.';
     console.log('The user answered: ' + answer1);
     }
+
+//along with the '||' operator, the user inputs are all converted to lower case
+//to make sure that 'no' and 'NO' are both processed as the same response
 
 var question2 = prompt('Have I ever been to Disneyland?').toLowerCase();
   if (question2 === 'no' || question2 ==='n') {
@@ -30,46 +37,43 @@ var question3 = prompt('Have I ever lived in North Dakota?').toLowerCase();
       answer3 = 'correctly. I lived in North Dakota for 6 months.';
       total++;
       console.log('The user answered: ' + answer3);
-      } else {
-      answer3 = 'wrong. I actually DID live in ND, but I only lasted 6 months before the excruciatingly boring geology sent me fleeing back to the PNW.';
+    } else {
+      answer3 = 'wrong. I actually DID live in ND, but I only lasted 6 months.';
        console.log('The user answered: ' + answer3);
       }
 
-//question4 numerical answer with 3 tries to answer correctly and hints after incorrect answers
-////var question4 = parseInt(prompt('How many years do you think I have lived in King county?'));
-  //if (question4 === 10) {
-    //answer4 = 'correctly. I have lived here since the day after I graduated high school.';
-    //total++;
-    //console.log('The user answered: ' + answer4);
-    //} else if (question4 <= 9) {
-      //answer4 = 'wrong. You\'re a little low.';
-      //console.log('The user answered: ' + answer4);
-      //} else if (question4 >= 11) {
-        //  answer4 = 'wrong. You\'re a bit over.';
-          //console.log('The user answered: ' + answer4);
-//        }
-//var triesRemaining = 3;
-//var question4 = 10;
-  //do {
-    //var answer4 = prompt('How many years do you think I have been living in King County?');
-      //if (answer4 !== 10);
-      //{
-        //triesRemaining--;
-        //alert()
-      //}
-    //}
-//var  = prompt(‘guess my number!’);
-  //if (userInput  === correctAnswer ) {
-    //alert(“wow! you’re right!’);
-    //} else {
-  //if ( userInput < correctAnswer ) {
-    //alert(‘too low!’);
-    //} else {
-    //alert(‘too high!’);
-    //}
-  //}
+//question4 numerical answer with a do...while loop let's the user know
+//if they need to guess higher or lower and the loop discontinues after 3 guesses.
 
-//question 5 -- yes or no
+var triesRemaining = 3;
+  do {
+      var question4 = parseInt(prompt('How many years do you think I have lived in King county?'));
+      if (question4 === 10) {
+        answer4 = 'correctly. I have lived here since the day after I graduated high school.';
+        total++;
+        console.log('The user answered: ' + answer4);
+      } else if (question4 <= 9) {
+          answer4 = 'Wrong. Longer than that... guess again!';
+          console.log('The user answered: ' + answer4);
+        } else if (question4 >= 11) {
+            answer4 = 'Wrong. Sadly, I have not been here quite that long. Guess again!';
+            console.log('The user answered: ' + answer4);
+          }
+      if (question4 !== 10){
+        triesRemaining--;
+        alert(answer4);
+      } else {
+           break;
+        }
+  } while (triesRemaining > 0)
+
+  if (triesRemaining <= 0){
+    alert('Sorry, you\'re out of guesses.');
+  }
+
+
+//question 5 -- yes or no, if else statement with an 'or' operator to account
+//for different ways the user might answer correctly
 
 var question5 = prompt('Did I meet my husband online?').toLowerCase();
   if (question5 === 'yes' || question5 === 'y') {
@@ -77,11 +81,12 @@ var question5 = prompt('Did I meet my husband online?').toLowerCase();
     total++;
     console.log('The user answered: ' + answer5);
   } else {
-    answer5 = 'wrong. We were both too musy all the time to try and meet people. Yay internet!';
+    answer5 = 'wrong. We were both too busy all the time to try and meet people. Yay internet!';
     console.log('The user answered: ' + answer5);
   }
 
-//question 6 -- numeric, no wiggle room... they already had a question with multiple guesses allowed.
+//question 6 -- numeric, no wiggle room... they already had a
+//question with multiple guesses allowed.
 
 var question6 = parseInt(prompt('How many volcanoes are there in Washington State?'));
   if (question6 === 5) {
@@ -89,7 +94,7 @@ var question6 = parseInt(prompt('How many volcanoes are there in Washington Stat
     total++;
     console.log('The user answered: ' + answer6);
   } else {
-    answer6 = 'wrong. We have a lot of really cool geology here and you should definitely check it out.';
+    answer6 = 'wrong. We have 5! Mount Rainier, Mt. St Helens, Mount Adams, Mount Baker, and Glacier Peak.';
     console.log('The user answered: ' + answer6);
   }
 
@@ -99,6 +104,8 @@ var user = prompt('What is your name?');
   alert('Welcome to my page ' + user + '. ' + 'You answered ' +  total + ' questions right!');
   console.log('The player is named: ' + user);
 
+//these alerts appear after the questions are asked and the user is welcomed to the page
+//the correct answers for each question are in these alerts
   alert('You answered question 1 ' + answer1);
   alert('You answered question 2 ' + answer2);
   alert('You answered question 3 ' + answer3);
